5981ec826538ec1cfbcf9a2dbfdcf2b950837d96
consoledot <bimbooladele@outlook.com>
2021-07-28 00:02:19 +0100

    added initliaze npm

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
 
 const post = {};
 app.get("/post", (req, res) => {});
 
-app.post("/post", (req, res) => {});
+app.post("/post", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const title = req.body;
+  post[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(post[id]);
+});
 
 app.listen(4000, () => console.log("Listening on 4000"));
