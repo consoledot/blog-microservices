dd12f6f3fefc7849e9bda282503a8326645308ec
consoledot <bimbooladele@outlook.com>
2021-08-01 17:38:49 +0100

    created event bus directory

--- comments/index.js
+++ comments/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
-const bodyParser = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
-const comments = {};
+app.use(cors());
 
-app.get("/post/:id/comments", (req, res) => {});
+const commentsByPostId = {};
 
-app.post("/post/:id/comments", (req, res) => {});
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
-app.listen(4001, () => console.log("listening on port 5000"));
+app.post("/posts/:id/comments", (req, res) => {
+  const { content } = req.body;
+  const commentsId = randomBytes(4).toString("hex");
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentsId, content });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => console.log("listening on port 4001"));
