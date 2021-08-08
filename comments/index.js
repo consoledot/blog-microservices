a2fe5b1bcf90855735baf3a01d2f2a2fbdd36421
consoledot <bimbooladele@outlook.com>
2021-08-08 21:30:48 +0100

    made several changes

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,13 +13,24 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const { content } = req.body;
-  const commentsId = randomBytes(4).toString("hex");
+  const commentId = randomBytes(4).toString("hex");
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentsId, content });
+  comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
   res.status(201).send(comments);
 });
-
+app.post("/events", (req, res) => {
+  console.log("Received", req.body.type);
+  res.send({});
+});
 app.listen(4001, () => console.log("listening on port 4001"));
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,14 +13,22 @@ app.get("/posts", (req, res) => {
   res.status(200).send(post);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   post[id] = {
     id,
     title,
   };
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
   res.status(201).send(post[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Received", req.body.type);
+  res.send({});
+});
 app.listen(4000, () => console.log("Listening on port 4000"));
