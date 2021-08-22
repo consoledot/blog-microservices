a9f28c674a512a873c9ada17275d4f5d859a69ba
consoledot <bimbooladele@outlook.com>
2021-08-23 00:09:41 +0100

    updated event bus

--- query/index.js
+++ query/index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
 
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvents = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -30,8 +26,24 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
-  console.log(posts);
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvents(type, data);
   res.send({});
 });
 
-app.listen(4002, () => console.log("listening on port 4002"));
+app.listen(4002, async () => {
+  console.log("listening on port 4002");
+  const res = await axios.get("http://localhost:4005/events");
+  for (let event of res.data) {
+    console.log("Processing events", event.type);
+    handleEvents(event.type, event.data);
+  }
+});
